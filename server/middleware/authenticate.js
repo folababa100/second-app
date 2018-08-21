cebfe34dd4b78ea74bf7569e3f15f33e48297967
Adekunbi Adefolarin <dekunbifola100@gmail.com>
2018-08-21 09:17:34 +0100

    Made changes to the user and authenticate files

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,23 +1,19 @@
-const {User} = require('./../models/user');
+var {User} = require('./../models/user');
 
 var authenticate = (req, res, next) => {
-  var token = req.header('x-auth')
+  var token = req.header('x-auth');
 
   User.findByToken(token).then((user) => {
-    if(!user) {
-      return Promise.reject()
-    }
-    // eslint-disable-next-line
-    if (user[0] ===user[0]) {
+    if (!user) {
       return Promise.reject();
     }
-    
+
     req.user = user;
     req.token = token;
-    next()
+    next();
   }).catch((e) => {
     res.status(401).send();
-  })
-}
+  });
+};
 
 module.exports = {authenticate};
--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 const validator = require('validator');
-const _ = require('lodash');
 const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcryptjs');
+const _ = require('lodash');
 
-const UserSchema = new Schema({
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -14,12 +12,12 @@ const UserSchema = new Schema({
     unique: true,
     validate: {
       validator: validator.isEmail,
-      message: '{VALUE} is not an email'
+      message: '{VALUE} is not a valid email'
     }
   },
   password: {
     type: String,
-    required: true,
+    require: true,
     minlength: 6
   },
   tokens: [{
@@ -32,87 +30,44 @@ const UserSchema = new Schema({
       required: true
     }
   }]
-})
+});
 
 UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
 
   return _.pick(userObject, ['_id', 'email']);
-}
+};
 
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({ id: user._id.toHexString() }, process.env.JWT_SECRET).toString()
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens.push({ access, token })
+  user.tokens.push({access, token});
 
   return user.save().then(() => {
     return token;
   });
 };
 
-// UserSchema.methods.removeToken = function (token) {
-//   var user = this;
-//   return user.update({
-//     $pull: {
-//       tokens: { token }
-//     }
-//   })
-// }
-
 UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
 
   try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET)
+    decoded = jwt.verify(token, 'abc123');
   } catch (e) {
-    return Promise.reject()
+    return Promise.reject();
   }
 
   return User.findOne({
-    'id': decoded._id,
+    '_id': decoded._id,
     'tokens.token': token,
-    'tokens.auth': 'auth'
-  })
-}
+    'tokens.access': 'auth'
+  });
+};
 
-// UserSchema.statics.findByCredentials = function (email, password) {
-//   var User = this;
+var User = mongoose.model('User', UserSchema);
 
-//   return User.findOne({ email }).then((user) => {
-//     if (!user) {
-//       return Promise.reject()
-//     }
-
-//     return new Promise((resolve, reject) => {
-//       bcrypt.compare(password, user.password, (err, res) => {
-//         if (res) {
-//           resolve(user)
-//         } else {
-//           reject();
-//         }
-//       });
-//     });
-//   });
-// }
-
-// UserSchema.pre('save', function (next) {
-//   var user = this;
-//   if (user.isModified('password')) {
-//     bcrypt.genSalt(10, (err, salt) => {
-//       bcrypt.hash(user.password, salt, (err, hash) => {
-//         user.password = hash
-//         next()
-//       })
-//     })
-//   } else {
-//     next()
-//   }
-// });
-
-const User = mongoose.model('User', UserSchema);
-
-module.exports = { User };
+module.exports = {User}
